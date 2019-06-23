9d9b4947195269f25074161abb0f0b58e39a273d
tushartripathi1998 <thetushartripathi1998@gmail.com>
2019-06-23 14:55:02 +0530

    showing indented data

--- app/src/main/java/com/example/myapiapplication/fetchData.java
+++ app/src/main/java/com/example/myapiapplication/fetchData.java
@@ -2,6 +2,10 @@ package com.example.myapiapplication;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,6 +17,9 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class fetchData extends AsyncTask<Void, Void, Void> {
     String data = "";
+    String dataParsed = "";
+    String singleParsed = "";
+
     @Override
     protected Void doInBackground(Void... voids) {
 
@@ -27,9 +34,22 @@ public class fetchData extends AsyncTask<Void, Void, Void> {
                 line = bufferedReader.readLine();
                 data = data+line;
             }
+
+            JSONArray jsonArray = new JSONArray(data);
+            for(int i=0;i<jsonArray.length();i++){
+                JSONObject jsonObject = (JSONObject)jsonArray.get(i);
+                singleParsed = "Name:" + jsonObject.get("name")+"\n"+
+                               "Password:" + jsonObject.get("password")+"\n"+
+                               "Contact:" + jsonObject.get("contact")+"\n"+
+                               "Country:" + jsonObject.get("country")+"\n";
+
+                dataParsed = dataParsed + singleParsed;
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
@@ -39,6 +59,6 @@ public class fetchData extends AsyncTask<Void, Void, Void> {
     @Override
     protected void onPostExecute(Void aVoid) {  //on ui work, and showing the data
         super.onPostExecute(aVoid);
-        MainActivity.data.setText(data);
+        MainActivity.data.setText(dataParsed);
     }
 }
